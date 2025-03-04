3e99d2c645fb80b2c211e76e247c1317e408bdee
diealex <serendipisland@gmail.com>
2025-03-04 17:49:32 +0100

    goit js 08

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const textInput = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
-
-textInput.addEventListener("input", (event) => {
-  let str = event.currentTarget.value;
-  output.textContent = !str?.trim() ? "Anonymous" : str.trim();
-});
